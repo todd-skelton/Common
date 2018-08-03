bed0f31ed4aad08555934ca9509e084a51c6e484
Todd Skelton <todd.skelton@outlook.com>
2018-08-03 00:53:29 -0500

    Create default implementations for CQRS

---
+++ Domain.Cqrs/CommandDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kloc.Common.Domain.Cqrs
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        public async Task Send<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            var obj = _serviceProvider.GetService(typeof(ICommandHandler<TCommand>));
+
+            if (obj is ICommandHandler<TCommand> handler)
+                await handler.HandleAsync(command);
+            else
+                throw new ArgumentException($"{typeof(TCommand).FullName} does not have a valid handler of type {typeof(ICommandHandler<TCommand>).FullName} registered.");
+        }
+    }
+}
---
+++ Domain.Cqrs/QueryDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kloc.Common.Domain.Cqrs
+{
+    public class QueryDispatcher : IQueryDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public QueryDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        public async Task<TResult> SendAsync<TQuery, TResult>(TQuery query) where TQuery : IQuery<TResult>
+        {
+            var obj = _serviceProvider.GetService(typeof(IQueryHandler<TQuery, TResult>));
+
+            if (obj is IQueryHandler<TQuery, TResult> handler)
+            {
+                return await handler.HandleAsync(query);
+            }
+            else
+            {
+                throw new ArgumentException($"{typeof(TQuery).FullName} does not have a valid handler of type {typeof(IQueryHandler<TQuery, TResult>).FullName} registered.");
+            }
+        }
+    }
+}
