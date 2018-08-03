3fd56613793c39b80ec4ba2354c4f64a5d6b4a1a
Todd Skelton <todd.skelton@outlook.com>
2018-08-03 12:02:12 -0500

    Create default exception handler

---
+++ Excepting/DefaultExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kloc.Common.Excepting
+{
+    public class DefaultExceptionHandler : IExceptionHandler
+    {
+        public Task HandleAsync(Exception exception, params object[] args)
+        {
+            throw new Exception("Encountered exception.", exception);
+        }
+
+        public Task<TResult> HandleAsync<TResult>(Exception exception, params object[] args)
+        {
+            throw new Exception("Encountered exception.", exception);
+        }
+    }
+}
