1176da932743700b1ba5439476e52901d388a9cb
Todd Skelton <todd.skelton@outlook.com>
2018-09-11 13:30:08 -0500

    Create blockchain library

---
+++ Blockchaining/Block.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+
+namespace Kloc.Common.Blockchaining
+{
+    public class Block<T>
+    {
+        private Block()
+        {
+        }
+
+        public Block(Block<T> previousBlock, T data)
+        {
+            Id = previousBlock == null ? 0 : previousBlock.Id + 1;
+            PreviousHash = previousBlock?.Hash;
+            Timestamp = DateTimeOffset.Now;
+            Data = data;
+            Hash = CalculateHash();
+        }
+
+        public long Id { get; private set; }
+        public DateTimeOffset Timestamp { get; private set; }
+        public string PreviousHash { get; private set; }
+        public string Hash { get; private set; }
+        public T Data { get; private set; }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] outputBytes = sha256.ComputeHash(GetBytes());
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        private byte[] GetBytes()
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, Id);
+                formatter.Serialize(ms, Timestamp);
+                if (PreviousHash != null) formatter.Serialize(ms, PreviousHash);
+                if (Data != null) formatter.Serialize(ms, Data);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Blockchaining/Blockchain.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Kloc.Common.Blockchaining
+{
+    public class Blockchain<T>
+    {
+        public List<Block<T>> _chain { get; private set; }
+
+        public Blockchain()
+        {
+            _chain = new List<Block<T>>
+            {
+                new Block<T>(null, default)
+            };
+        }
+
+        public IReadOnlyCollection<Block<T>> Chain => _chain.AsReadOnly();
+
+        public void Add(T item)
+        {
+            var latestBlock = _chain[_chain.Count - 1];
+            var block = new Block<T>(latestBlock, item);
+            _chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                var currentBlock = _chain[i];
+                var previousBlock = _chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public Block<T> this[int i] => _chain[i];
+    }
+}
